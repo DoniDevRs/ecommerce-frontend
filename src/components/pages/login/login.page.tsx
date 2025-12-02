2263b4feedccebfcd138bf12c242c4c15bbe4a91
Doni Ramos da Silva <doni.silva@goias.gov.br>
2025-12-02 10:30:33 -0300

    feat: add error messages to Login Pages and custom email validation.

--- src/components/pages/login/login.page.tsx
+++ src/components/pages/login/login.page.tsx
@@ -1,6 +1,7 @@
 import { BsGoogle } from "react-icons/bs";
 import { FiLogIn} from "react-icons/fi";
 import { useForm } from "react-hook-form";
+import validator from "validator";
 
 // Components
 import CustomButton from "../../custom-button/custom-button.component";
@@ -9,7 +10,7 @@ import Header from "../../header/header.component";
 // Styles
 import { LoginContainer, LoginHeadline, LoginInputContainer, LoginSubtitle, LoginContent } from "./login.styles";
 import CustomInput from "../../custom-input/custom-input.component";
-import { data } from "react-router-dom";
+import InputErrorMessage from "../../input-error-message/input-error-message.component";
 
 const LoginPage = () => {
     const { register,
@@ -35,7 +36,14 @@ const LoginPage = () => {
                 <CustomInput 
                 hasError={!!errors?.email} 
                 type="email" placeholder="Digite seu e-mail" 
-                {...register("email", {required: true})} />
+                {...register("email", {required: true, validate: (value) => {
+                    return validator.isEmail(value);
+                }})} />
+
+                {errors?.email?.type === "required" && (<InputErrorMessage>E-mail é obrigatório</InputErrorMessage>)}
+
+                {errors?.email?.type === "validate" && (<InputErrorMessage>E-mail inválido</InputErrorMessage>)}
+
             </LoginInputContainer>
             <LoginInputContainer> 
                 <p>Senha</p>
@@ -43,6 +51,9 @@ const LoginPage = () => {
                 hasError={!!errors?.password} 
                 type="password" placeholder="Digite sua senha" 
                 {...register("password", {required: true})} />
+
+            {errors?.password?.type === "required" && (<InputErrorMessage>Senha é obrigatória</InputErrorMessage>)}
+
             </LoginInputContainer>
 
              <CustomButton startIcon={<FiLogIn size={18}/>} onClick={() =>handleSubmit(handleSubmitPress)()}> Entrar </CustomButton>
