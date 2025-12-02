69a00f778004b32c81ef46a2e6b70fb6a08abd3e
Doni Ramos da Silva <doni.silva@goias.gov.br>
2025-12-02 10:16:47 -0300

    feat: add React Hook validation to Login Page.

--- src/components/pages/login/login.page.tsx
+++ src/components/pages/login/login.page.tsx
@@ -1,5 +1,6 @@
 import { BsGoogle } from "react-icons/bs";
 import { FiLogIn} from "react-icons/fi";
+import { useForm } from "react-hook-form";
 
 // Components
 import CustomButton from "../../custom-button/custom-button.component";
@@ -8,8 +9,15 @@ import Header from "../../header/header.component";
 // Styles
 import { LoginContainer, LoginHeadline, LoginInputContainer, LoginSubtitle, LoginContent } from "./login.styles";
 import CustomInput from "../../custom-input/custom-input.component";
+import { data } from "react-router-dom";
 
 const LoginPage = () => {
+    const { register,
+            formState: { errors }, handleSubmit } = useForm();
+
+    const handleSubmitPress = (data: any) => {
+        console.log(data);
+    }      
     return (
       <>
         <Header />
@@ -24,14 +32,20 @@ const LoginPage = () => {
 
             <LoginInputContainer> 
                 <p>E-mail</p>
-                <CustomInput type="email" placeholder="Digite seu e-mail" />
+                <CustomInput 
+                hasError={!!errors?.email} 
+                type="email" placeholder="Digite seu e-mail" 
+                {...register("email", {required: true})} />
             </LoginInputContainer>
             <LoginInputContainer> 
                 <p>Senha</p>
-                <CustomInput type="password" placeholder="Digite sua senha" />
+                <CustomInput 
+                hasError={!!errors?.password} 
+                type="password" placeholder="Digite sua senha" 
+                {...register("password", {required: true})} />
             </LoginInputContainer>
 
-             <CustomButton startIcon={<FiLogIn size={18}/>}> Entrar </CustomButton>
+             <CustomButton startIcon={<FiLogIn size={18}/>} onClick={() =>handleSubmit(handleSubmitPress)()}> Entrar </CustomButton>
           </LoginContent>
         </LoginContainer>
       </>
