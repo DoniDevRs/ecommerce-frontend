21c10ba735f529f2772cc8b0926143106a2887a7
Doni Ramos da Silva <doni.silva@goias.gov.br>
2025-12-03 07:50:51 -0300

    chore: initialize firebase auth

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,6 +1,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCDmbGARjId3X7HsYCaXWW906XHS__tp7M",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
