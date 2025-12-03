f55d0f0caf67ea3493226b313530b9d29209180d
Doni Ramos da Silva <doni.silva@goias.gov.br>
2025-12-03 08:40:05 -0300

    feat: add auth state listener and sign out button

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/pages/home/home.page";
 import LoginPage from "./components/pages/login/login.page";
 import SignUpPage from "./components/pages/sign-up/sign-up.page";
+import { onAuthStateChanged } from "firebase/auth";
+
+//Utilities
+import { auth } from "./config/firebase.config";
 
 const App: FunctionComponent = () => {
+  onAuthStateChanged(auth, (user) => {
+    console.log(user);
+  })
+
   return (
     <BrowserRouter>
       <Routes>
--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -2,6 +2,8 @@ import { BsCart3} from 'react-icons/bs'
 import { useNavigate } from 'react-router-dom';
 
 import { HeaderContainer, HeaderItems, HeaderItem, HeaderTitle } from './header.styles';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../config/firebase.config';
 
 const Header = () => {
   const navigate = useNavigate();
@@ -23,6 +25,7 @@ const Header = () => {
           <HeaderItem>Explorar</HeaderItem>
           <HeaderItem onClick={handleLoginClick}>Login</HeaderItem>
           <HeaderItem onClick={handleSignUpClick}>Criar conta</HeaderItem>
+          <HeaderItem onClick={() => signOut(auth)}>Sair</HeaderItem>
           <HeaderItem>
             <BsCart3 size={25} />
             <p style={ { marginLeft: 5 } }> 5</p>
