6b6877a6b72169b32fcc3628c867c650e938f06d
Doni Ramos da Silva <doni.silva@goias.gov.br>
2025-12-04 08:26:44 -0300

    feat: add Loading to Categories Overview.

--- src/components/categories-overview/categories-overview.component.tsx
+++ src/components/categories-overview/categories-overview.component.tsx
@@ -8,15 +8,18 @@ import { CategoryContext } from "../../contexts/category.context";
 
 //Components
 import CategoryOverview from "../category-overview/category-overview.component";
+import LoadingComponent from "../loading/loading.component";
 
 const CategoriesOverview: FunctionComponent = () => {
-    const { categories, fetchCategories } = useContext(CategoryContext);
+    const { categories, fetchCategories, isLoading } = useContext(CategoryContext);
 
     useEffect(() => {
         if (categories.length === 0) {
             fetchCategories();
         }
     }, []);
+
+    if (isLoading) { return <LoadingComponent />; }
 
     return (
         <Container>
