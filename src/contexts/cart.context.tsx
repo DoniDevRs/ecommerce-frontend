e6980a363bb2139917abc45267446969669722ab
Doni Ramos da Silva <doni.silva@goias.gov.br>
2025-12-04 13:11:34 -0300

    faet: handle multiple products on cart of the same id.

--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -25,10 +25,23 @@ const CartContextProvider: FunctionComponent<{ children: ReactNode }> = ({ child
         setIsVisible((prevState) => !prevState);
     }
 
-    const addProductToCart = (product: Product) => { 
-        setProducts(prevState => [...prevState, { ...product, quantity: 1 }])
+    const addProductToCart = (product: Product) => {
+      const productIsAlreadyInCart = products.some(
+        (item) => item.id === product.id
+      );
 
-    }
+      if (productIsAlreadyInCart) {
+        return setProducts((products) =>
+          products.map((item) =>
+            item.id === product.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          )
+        );
+      }
+
+      setProducts((prevState) => [...prevState, { ...product, quantity: 1 }]);
+    };
 
     return (
         <CartContext.Provider value={{ isVisible, products, toogleCart, addProductToCart }}> 
